4b1c87e506005da019fe1cebc76c0779972a837e
AntonWangDTU <143256553+AntonWangDTU@users.noreply.github.com>
2026-03-02 17:07:41 +0000

    Deploying to gh-pages from @ AntonWangDTU/AntonWangDTU.github.io@8911c78c2b91b8be6a468bd91540224ab9499553 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,35 +105,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/rendercv/rendercv_output/Anton_Wang_Strandberg_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6E%74%6F%6E.%77.%73%74%72%61%6E%64%62%65%72%67@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://www.linkedin.com/in/Anton Wang Strandberg", "_blank");
         },
       },{
       id: 'light-theme',
