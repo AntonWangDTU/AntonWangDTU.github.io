4a0d086a9ed6bbfb6facc8cb20e43a5fac1fede3
AntonWangDTU <143256553+AntonWangDTU@users.noreply.github.com>
2026-03-09 11:40:28 +0000

    Deploying to gh-pages from @ AntonWangDTU/AntonWangDTU.github.io@a208221d1012cc870393d6fecc444e074b44ef7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -119,6 +119,11 @@ ninja.data = [{
           description: "My rust coding journey",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_Rust/";
+            },},{id: "projects-computer-vision-project",
+          title: 'Computer vision project',
+          description: "A computer vision project i did at DTU for microscopic cell recognition.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_DL_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
